3cbc4ad4578edec4f7cff8f2eb49a7a51cfe885a
Yu <tatataabcd@gmail.com>
2024-12-20 01:33:31 +0900

    Revert "chatgpt-4o-latest"
    This reverts commit 759197b420f2a414aef1fdfe3b210d3d227a299b.

--- lib/chat-setting-limits.ts
+++ lib/chat-setting-limits.ts
@@ -151,7 +151,7 @@ export const CHAT_SETTING_LIMITS: Record<LLMID, ChatSettingLimits> = {
     MAX_TOKEN_OUTPUT_LENGTH: 4096,
     MAX_CONTEXT_LENGTH: 8192
   },
-  "chatgpt-4o-latest": {
+  "gpt-4o": {
     MIN_TEMPERATURE: 0.0,
     MAX_TEMPERATURE: 2.0,
     MAX_TOKEN_OUTPUT_LENGTH: 4096,
